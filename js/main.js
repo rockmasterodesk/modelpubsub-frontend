dc7fd90b9f97f18afa66dd7da811da612b6ab01a
ASM Asaduzzaman <rockmasterodesk@gmail.com>
2023-07-20 01:02:05 +0600

    Responsiveness

--- js/main.js
+++ js/main.js
@@ -1,13 +1,58 @@
-const body = document.querySelector('body'),
-    modeSwitch = body.querySelector(".toggle-switch");
+// START: Global Variables
 
+const body        = document.querySelector('body');
+
+// END: Global Variables
+
+
+
+
+
+
+
+// START: Theme Mode (light or dark theme related scripts)
+
+// If the localStorage has "theme_mode" key, then get the value from localStorage
+if (localStorage.getItem("theme_mode")) {
+    const themeMode = localStorage.getItem("theme_mode");
+
+    if (themeMode === "light") {
+        body.classList.remove("dark");
+    }
+}
+
+const modeSwitch  = body.querySelector(".toggle-switch");
 modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
+    // If the body contains "dark" class, then remove it and add "light" class
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        localStorage.setItem("theme_mode", "light");
+    }
+    // Else, if the body contains "light" class, then remove it and add "dark" class
+    else {
+        body.classList.add("dark");
+        localStorage.setItem("theme_mode", "dark");
+    }
 });
+
+// END: Theme Mode (light or dark theme related scripts)
+
+
+
+
+
+
+
+// START: Bootstrap Tooltips Related Scripts
 
 // Initialize Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// END: Bootstrap Tooltips Related Scripts
+
+
+
 
 
 
